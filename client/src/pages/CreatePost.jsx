2735447b38250d4808b9b1cfe8db2a802f6eafe2
Nishiv Singh <singh.136@iitj.ac.in>
2023-08-03 01:00:44 +0530

    Getting and Posting data using claudinary and added searching filter functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,7 +16,32 @@ const CreatePost = () => {
   const [generatingImage, setGeneratingImage] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        });
+
+        await response.json();
+        navigate("/");
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert("Please enter a prompt");
+    }
+  };
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -31,14 +56,12 @@ const CreatePost = () => {
       try {
         setGeneratingImage(true);
         const response = await fetch("http://localhost:8080/api/v1/openai", {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({ prompt: form.prompt }),
         });
-
-        console.log(response);
         const data = await response.json();
 
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Card, FormField, Loader } from "../components";
 
 const RenderCards = ({ data, title }) => {
-  if (data?.length) {
+  if (data?.length > 0) {
     return data.map((post) => <Card key={post._id} {...post} />);
   }
   return (
@@ -13,7 +13,46 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+
+    };
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+
+        setSearchedResults(searchResults);
+      }, 500)
+    );
+  }
   return (
     <section className="max-w-7xl mx-auto">
       <div className="">
@@ -27,7 +66,7 @@ const Home = () => {
       </div>
 
       <div className="mt-16">
-        <FormField />
+        <FormField labelName="Search posts" type="text" name="text" placeholder="Search posts" value={searchText} handleChange={handleSearchChange} />
       </div>
       <div className="mt-10">
         {loading ? (
@@ -44,9 +83,9 @@ const Home = () => {
             )}
             <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xs:grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards data={[]} title="No search results found!" />
+                <RenderCards data={searchedResults} title="No search results found!" />
               ) : (
-                <RenderCards data={[]} title="No posts found" />
+                <RenderCards data={allPosts} title="No posts found" />
               )}
             </div>
           </>
